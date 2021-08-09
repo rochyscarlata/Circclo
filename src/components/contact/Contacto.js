d49c84e5e57148f973410ef94eaba130a0628f66
rochyscarlata <rosarioscarlata24@gmail.com>
2021-08-09 17:41:56 -0300

    corrigiendo contact

--- src/components/contact/Contacto.js
+++ src/components/contact/Contacto.js
@@ -58,9 +58,7 @@ const Contacto = () => {
           <Form.Group className="" controlId="exampleForm.ControlInput1">
             <Form.Control type="email" placeholder="Email" />
           </Form.Group>
-          <Form.Group className="" controlId="exampleForm.ControlInput1">
-            <Form.Control type="name" placeholder="Nombre y apellido" />
-          </Form.Group>
+        
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Control as="textarea" rows={3}  placeholder="Mensaje"/>
           </Form.Group>
